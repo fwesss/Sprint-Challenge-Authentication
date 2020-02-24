f323d471cae069a52f58f6ea78ca114fb5055f22
Westley Feller <feller.westley@gmail.com>
2020-02-23 18:28:33 -0800

    Register new user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,41 @@
-const router = require('express').Router();
+import { Router } from 'express'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
+import jwtSecret from '../secrets'
+import Users from '../users/users.model'
+
+const router = Router()
+
+const generateToken = ({ id, username }) =>
+  jwt.sign(
+    {
+      subject: id,
+      username,
+    },
+    jwtSecret,
+    {
+      expiresIn: '1d',
+    }
+  )
+
+router.post('/register', async (req, res) => {
+  const user = req.body
+  const hash = bcrypt.hashSync(user.password, 10)
+  const hashedUser = { ...user, password: hash }
+
+  try {
+    const registeredUser = await Users.insert(hashedUser)
+    res
+      .status(201)
+      .json({ user: registeredUser, token: generateToken(registeredUser) })
+  } catch (error) {
+    res.status(500).json({ message: 'User registration failed.', error })
+  }
+})
 
 router.post('/login', (req, res) => {
   // implement login
-});
+})
 
-module.exports = router;
+export default router
---
+++ secrets.js
@@ -0,0 +1,1 @@
+export default process.env.JWT_SECRET || 'yo yo yo yo yo'
