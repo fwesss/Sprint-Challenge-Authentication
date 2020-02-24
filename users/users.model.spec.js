86df83aa8effc69742041c3a7c7cbd3ce78d4dbc
Westley Feller <feller.westley@gmail.com>
2020-02-23 19:04:40 -0800

    Testing insert model method

--- users/users.model.spec.js
+++ users/users.model.spec.js
@@ -2,6 +2,28 @@ const db = require('../database/dbConfig')
 const { findBy, insert } = require('./users.model')
 
 describe('users model', () => {
+  describe('insert', () => {
+    it('should add a user to the database', async () => {
+      await insert({ username: 'Tuna', password: 'Treats' })
+      await insert({ username: 'Scout', password: 'Treats' })
+
+      expect(await findBy({ password: 'Treats' })).toHaveLength(2)
+    })
+
+    it('should return the inserted employee', async () => {
+      expect(
+        await insert({
+          username: 'Tuna',
+          password: 'Treats',
+        })
+      ).toEqual({
+        id: 1,
+        username: 'Tuna',
+        password: 'Treats',
+      })
+    })
+  })
+
   describe('findBy', () => {
     it('should return a list of 1 user with a username matching the request', async () => {
       await insert({ username: 'Tuna', password: 'Treats' })
